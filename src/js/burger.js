40dadb335caa403b25579c2e08fc989ce12c6e39
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-10-03 23:52:59 +0300

    feat: burger menu

---
+++ src/js/burger.js
@@ -0,0 +1,17 @@
+const refs = {
+  mobileMenu: document.querySelector("[data-menu]"),
+  mobileMenuButton: document.querySelector("[data-menu-button]"),
+  mobileMenuCloseButton: document.querySelector("[data-menu-close]"),
+  heroSection: document.querySelector("[data-hero]"),
+};
+
+refs.mobileMenuButton.addEventListener("click", burgerMobile);
+
+refs.mobileMenuCloseButton.addEventListener("click", burgerMobile);
+
+function burgerMobile() {
+  refs.mobileMenu.classList.toggle("-translate-x-full");
+  refs.heroSection.classList.toggle("hidden");
+  refs.mobileMenuButton.classList.toggle("hidden");
+  refs.mobileMenuCloseButton.classList.toggle("hidden");
+}
