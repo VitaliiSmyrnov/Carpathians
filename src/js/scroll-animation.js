fd205a68550d330720f48e8ea0773acf1017b750
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-10-20 18:59:25 +0300

    feat: scroll animation done

--- src/js/scroll-animation.js
+++ src/js/scroll-animation.js
@@ -5,9 +5,23 @@ function onEntry(entry) {
     }
   });
 }
+
 let options = { threshold: [0.5] };
 let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll(".element-animation");
-for (let elm of elements) {
+
+let topElements = document.querySelectorAll(".top-animation");
+for (let elm of topElements) {
+  observer.observe(elm);
+}
+let bottomElements = document.querySelectorAll(".bottom-animation");
+for (let elm of bottomElements) {
+  observer.observe(elm);
+}
+let leftElements = document.querySelectorAll(".left-animation");
+for (let elm of leftElements) {
+  observer.observe(elm);
+}
+let rightElements = document.querySelectorAll(".right-animation");
+for (let elm of rightElements) {
   observer.observe(elm);
 }
